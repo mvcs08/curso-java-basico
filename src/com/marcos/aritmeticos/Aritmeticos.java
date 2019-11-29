1e2b0e7577127eaebb3dbe3e585d2a029795482a
Marcos Vinicius <mdominique2011@gmail.com>
2019-11-29 03:26:04 -0300

    update Aritmeticos

--- src/com/marcos/aritmeticos/Aritmeticos.java
+++ src/com/marcos/aritmeticos/Aritmeticos.java
@@ -4,7 +4,32 @@ public class Aritmeticos {
 
 	public static void main(String[] args) {
 		
-
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		//Subtração
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		//Multiplicação
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		//Divisão
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		//Soma
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		//Módulo
+		resultado = resultado % 7;
+		System.out.println(resultado);
+	
+		//Imprime e depois soma
+		//4
+		System.out.println(resultado++);
+		
+		//Soma depois imprime
+		System.out.println(++resultado);
+		
+		
 	}
 
 }
